1f0ab2c01c1e21db8b7c686d435c490ec3ff0c1a
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2016-05-08 20:47:15 +0200

    more efficient link generation but requires exact name

--- linkmend.lua
+++ linkmend.lua
@@ -30,52 +30,28 @@ end
 
 function linkmend:mend_tags(text)
 	return string.gsub(text, '%b<>', function(tag)
-		local pattern = strsub(tag, 2, -2)
-		local matcher = self:fuzzy_matcher(strsub(tag, 2, -2))
-		local best_match
-		for item_id=1,20000 do
-			local name, _, quality = GetItemInfo('item:'..item_id)
-			if name then
-				local rating = matcher(name)
-				if rating and (not best_match or rating > best_match.rating or rating == best_match.rating and strlen(name) < strlen(best_match.name)) then
-					best_match = { rating = rating, item_id = item_id, name = name, quality = quality }
-				end
-			end
-		end
-		if best_match then
-			local color = strsub(({GetItemQualityColor(best_match.quality)})[4], 3)
-			return format(LINK_TEMPLATE, color, best_match.item_id, 0, 0, 0, best_match.name)
+		local name = strsub(tag, 2, -2)
+		local item_info = self.item_cache[strupper(name)]
+		if item_info then
+			local color = strsub(({GetItemQualityColor(item_info.quality)})[4], 3)
+			return format(LINK_TEMPLATE, color, item_info.item_id, 0, 0, 0, item_info.name)
 		else
 			return format('[%s]', pattern)
 		end
 	end)
 end
 
-function linkmend:fuzzy_matcher(input)
-	local uppercase_input = strupper(input)
-	local pattern = '(.*)'
-	for i=1,strlen(uppercase_input) do
-		if strfind(strsub(uppercase_input, i, i), '%w') or strfind(strsub(uppercase_input, i, i), '%s') then
-			pattern = pattern .. strsub(uppercase_input, i, i) .. '(.-)'
- 		end
-	end
-	return function(candidate)
-		local match = { strfind(strupper(candidate), pattern) }
-		if match[1] then
-			local rating = 0
-			for i=4,getn(match)-1 do
-				if strlen(match[i]) == 0 then
-					rating = rating + 1
-				end
- 			end
-			return rating
- 		end
-	end
-end
-
 function linkmend:ADDON_LOADED()
 	if arg1 ~= 'linkmend' then
 		return
+	end
+
+	self.item_cache = {}
+	for item_id=1,20000 do
+		local name, _, quality = GetItemInfo('item:'..item_id)
+		if name then
+			self.item_cache[strupper(name)] = { item_id = item_id, name = name, quality = quality }
+		end
 	end
 
 	local orig_ChatFrame_OnEvent = ChatFrame_OnEvent
